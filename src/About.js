3f320da57a3d7c9a28bdfc3bc910d3e859fdcd39
Mayank Singh <mayank@turningcloud.in>
2023-07-08 19:24:25 +0530

    note craft first commit

---
+++ src/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <p>"NoteCraft is a versatile and intuitive notes app designed to empower your productivity and organization. Seamlessly create, edit, search, and filter your notes with ease, allowing you to capture your thoughts, ideas, and important information effortlessly.</p>
+            <p>With NoteCraft, the process of note-taking becomes a delightful experience. Its sleek and user-friendly interface ensures a smooth workflow, enabling you to focus on the content that matters most. Whether you're jotting down quick reminders, drafting detailed project plans, or simply capturing moments of inspiration, NoteCraft offers the flexibility and tools you need.</p>
+            <p>Organizing your notes has never been simpler. Leverage the powerful search and filtering features to quickly locate specific notes based on keywords, tags, or categories. Say goodbye to endless scrolling and lost information – NoteCraft keeps your notes neatly organized and easily accessible at all times.</p>
+            <p>Collaboration is made effortless with NoteCraft. Share your notes with colleagues, friends, or family, allowing for seamless collaboration and real-time updates. Stay connected and work together to achieve your goals, whether it's planning an event, managing a team project, or brainstorming ideas.</p>
+            <p>NoteCraft synchronizes seamlessly across all your devices, ensuring your notes are accessible whenever and wherever inspiration strikes. Whether you're on your computer, tablet, or smartphone, your notes are always at your fingertips, ready to be edited, expanded upon, or shared on the go.</p>
+            <p>Experience the power of efficient note-taking and unleash your productivity with NoteCraft. Streamline your workflow, declutter your mind, and bring your ideas to life with a notes app designed to empower your creativity and organization."</p>
+            <p>Feel free to customize and adapt this description as needed to suit your specific branding and messaging.</p>
+        </div>
+    )
+}
+
+export default About
---
+++ src/Home.js
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+
+function Home() {
+
+    const [noteData, setNoteData] = useState({
+        title: "",
+        description: ""
+    });
+    const [search, setSearch] = useState("")
+
+    const [notesList, setNotesList] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [callGet, setCallGet] = useState(false)
+
+    useEffect(() => {
+        getNotes();
+    }, [callGet])
+
+    const getNotes = async () => {
+        try {
+            const response = await axios.get('https://cloud-notes-znsm.onrender.com/api/todos/get', {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            if (response.status == 200) {
+                setNotesList(response.data);
+                setLoading(false);
+            }
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+
+    const handleAddNote = async () => {
+        if (noteData.title !== "" && noteData.description !== "") {
+            setLoading(true);
+            try {
+                const response = await axios.post('https://cloud-notes-znsm.onrender.com/api/todos/add', noteData);
+                if (response.status == 200) {
+                    setNoteData({
+                        title: "",
+                        description: ""
+                    });
+                    setLoading(false);
+                    setCallGet(!callGet);
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        }
+        else {
+            alert("Please Fill title and discription")
+        }
+    }
+
+    const handleInput = (e) => {
+        let value = e.target.value, name = e.target.name;
+        setNoteData({
+            ...noteData,
+            [name]: value
+        });
+    }
+
+    const handleDelete = async (item) => {
+        setLoading(true);
+        console.log("ddddddd", item)
+        try {
+            const response = await axios.delete(`https://cloud-notes-znsm.onrender.com/api/todos/delete/${item._id}`);
+            if (response.status == 200) {
+                setLoading(false);
+                setCallGet(!callGet);
+            }
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+
+    return (
+        <div className='notes-container'>
+            <section className='nc-left-section'>
+                <form className='ncls-form'>
+                    {loading && <h1>Loading....</h1>}
+                    <h3>Write Your Note</h3>
+                    <div className='nclsf-field'>
+                        <label>Title</label>
+                        <input type="text" name="title" value={noteData.title} onChange={handleInput} />
+                    </div>
+                    <div className='nclsf-field'>
+                        <label>Description</label>
+                        <input placeholder='Write your note here' name="description" value={noteData.description} onChange={handleInput} />
+                    </div>
+                    <button type='button' onClick={handleAddNote} disabled={noteData.title == "" || noteData.description == ""}>Add Note</button>
+                </form>
+            </section>
+            <section className='nc-right-section'>
+                <h3>Notes Collection</h3>
+                <div className='ncrs-search'>
+                    <input type='text' placeholder='Search...' />
+                </div>
+                <ul>
+                    {notesList.length !== 0 ?
+                        notesList.map((item, index) =>
+                            <li key={index}>
+                                <h5>{item.title}</h5>
+                                <p>{item.description}</p>
+                                <div className='list-button'>
+                                    <button type='button' >Update</button>
+                                    <button type='button' className='delete' onClick={() => handleDelete(item)}>Delete</button>
+                                </div>
+                            </li>) :
+                        <li>No Data Found</li>
+                    }
+                </ul>
+            </section>
+        </div>
+    )
+}
+
+export default Home
